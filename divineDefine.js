6a9f4fe56c1d040565d764d7fd152b95204e617e
Kiran Mahajan <91893931+thekiranmahajan@users.noreply.github.com>
2023-08-31 07:35:09 +0530

    added a copy btn and responsive changes

--- divineDefine.js
+++ divineDefine.js
@@ -12,46 +12,64 @@ wordInput.addEventListener("keydown", (event) => {
 });
 
 searchBtn.addEventListener("click", () => {
-  let wordInput = document.getElementById("wordInput").value;
+  const wordInput = document.getElementById("wordInput").value;
   console.log(wordInput);
-
-  fetch(`${url}${wordInput}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      result.innerHTML = `  <div id="word">
+  if (!wordInput == "") {
+    fetch(`${url}${wordInput}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        result.innerHTML = `  <div id="word">
                 <h3>${wordInput}</h3><button onclick="pronounce()"><i class="fa-solid fa-volume-high"></i></button>
             </div>
             <div id="details">
                 <p>${data[0].meanings[0].partOfSpeech}</p>
                 <p>/${data[0].phonetic}/</p>
             </div>
-            <p id="wordMeaning">${
-              data[0].meanings[0].definitions[0].definition
-            }</p>
+            <div id="wordMeaning-copyBtn">
+                <p id="wordMeaning">${
+                  data[0].meanings[0].definitions[0].definition
+                }</p>
+                <button id="copyBtn"><i class="fa-solid fa-copy"></i></button>
+            </div>
             <p id="wordExample">${
               data[0].meanings[0].definitions[0].example ||
               data[0].meanings[1]?.definitions[0]?.example ||
               "null"
             }</p>`;
-      sound.setAttribute(
-        "src",
-        `${
-          data[0].phonetics[0]?.audio || //Optional Chaining (?.) Operator chatGPT's suggestion
-          data[0].phonetics[1]?.audio ||
-          data[0].phonetics[2]?.audio
-        }`
-      );
-      console.log(data);
-      console.log(sound);
-    })
-    .catch((error) => {
-      result.innerHTML = `<h3 id="errorMessage">Sorry pal, we couldn't find definitions for the word you were looking for.</h3>`;
-      console.log(error);
-      console.log(error.message);
-    });
+        sound.setAttribute(
+          "src",
+          `${
+            data[0].phonetics[0]?.audio || //Optional Chaining (?.) Operator chatGPT's suggestion
+            data[0].phonetics[1]?.audio ||
+            data[0].phonetics[2]?.audio
+          }`
+        );
+        const copyBtn = document.getElementById("copyBtn");
+        copyBtn.addEventListener("click", () => {
+          copyToClipboard(data[0].meanings[0].definitions[0].definition);
+        });
+      })
+      .catch((error) => {
+        result.innerHTML = `<h3 id="errorMessage">Sorry pal, we couldn't find definitions for the word you were looking for.</h3>`;
+        console.log(error);
+        console.log(error.message);
+      });
+  } else {
+    result.innerHTML = `<h3 id="errorMessage">Atleast provide me a word man!</h3>`;
+  }
 });
 const pronounce = () => {
   sound.play();
+};
+const copyToClipboard = (text) => {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log(`${text} is copied to clipboard!`);
+    })
+    .catch((error) => {
+      console.error("Copy failed:", error);
+    });
 };
